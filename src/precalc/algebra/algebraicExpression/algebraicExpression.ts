600a7a5129f2d7b8a470aac753ba295670bef580
Breno Alberto <brenoaps435@gmail.com>
2022-07-26 19:25:55 -0300

    feat: ensure AlgebraicExpressionService detects monomial expressions correctly

--- src/precalc/algebra/algebraicExpression/algebraicExpression.ts
+++ src/precalc/algebra/algebraicExpression/algebraicExpression.ts
@@ -2,10 +2,10 @@ export class AlgebraicExpressionService {
   private readonly operators: string[] = ['+', '-', '*', '/', '^']
 
   public getExpressionType (expression: string): string {
-    const expressionWithoutSpaces = this.removeSpaces(expression)
-
-    if (this.isNumericExpression(expressionWithoutSpaces)) {
+    if (this.isNumericExpression(expression)) {
       return 'numeric'
+    } else if (this.isMonomialExpression(expression)) {
+      return 'monomial'
     }
 
     return 'unknown'
@@ -15,13 +15,36 @@ export class AlgebraicExpressionService {
     return expression.replace(/\s/g, '')
   }
 
+  private removeNegation (expression: string): string {
+    let expWithoutNegation = expression
+    if (expWithoutNegation.startsWith('-')) { expWithoutNegation = expWithoutNegation.substring(1) }
+
+    const findNegationProceededByOperator = /[-+/*^](-)/g
+    if (findNegationProceededByOperator.test(expWithoutNegation)) {
+      expWithoutNegation = expWithoutNegation.replace(findNegationProceededByOperator, '$1')
+    }
+
+    return expWithoutNegation
+  }
+
   public getNumOfTerms (expression: string): number {
-    const terms = expression.split(/[-+/*]/)
+    const expWithoutSpaces = this.removeSpaces(expression)
+    const expWithoutNegation = this.removeNegation(expWithoutSpaces)
+    const terms = expWithoutNegation.split(/[-+/*]/)
     return terms.length
   }
 
   public isNumericExpression (expression: string): boolean {
-    const numericRegExp = /^(\d+(\.\d+)?[-+/*^]?)*$/
-    return numericRegExp.test(expression)
+    const expWithoutSpaces = this.removeSpaces(expression)
+    const numericRegExp = /^(-)?(\d+(\.\d+)?[-+/*^]?(-)?)*$/
+    return numericRegExp.test(expWithoutSpaces)
+  }
+
+  public isMonomialExpression (expression: string): boolean {
+    const expWithoutSpaces = this.removeSpaces(expression)
+    const numOfTerms = this.getNumOfTerms(expWithoutSpaces)
+    if (numOfTerms > 1) return false
+    const monomialExpRegExp = /^(-)?\d?[a-zA-Z]+([\^](-)?\d?[a-zA-Z]?)?$/
+    return monomialExpRegExp.test(expWithoutSpaces)
   }
 }
