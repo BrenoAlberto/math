17ea0102b2e9cb6c001e2ff930835abd895bc69e
Breno Alberto <brenoaps435@gmail.com>
2022-07-26 17:43:53 -0300

    test: ensure AlgebraicExpressionService detects numeric expressions number of terms correctly

--- src/precalc/algebra/algebraicExpression/algebraicExpression.spec.ts
+++ src/precalc/algebra/algebraicExpression/algebraicExpression.spec.ts
@@ -1,19 +1,42 @@
 import { AlgebraicExpressionService } from './algebraicExpression'
 
 const numericExpressionsExamples = [
-  '1',
-  '1 / 2',
-  '1 / 5 + 8',
-  '1^5',
-  '1^5 / 2^5'
+  {
+    str: '1',
+    numOfTerms: 1
+  },
+  {
+    str: '1 / 2',
+    numOfTerms: 2
+  },
+  {
+    str: '1 / 5 + 8',
+    numOfTerms: 3
+  },
+  {
+    str: '1^5',
+    numOfTerms: 1
+  },
+  {
+    str: '1^5 / 2^5',
+    numOfTerms: 2
+  }
 ]
 
 describe('AlgebraicExpressionService', () => {
   test('Should detect numeric expressions correctly', async () => {
     const sut = new AlgebraicExpressionService()
     for (const numericExp of numericExpressionsExamples) {
-      const result = sut.getExpressionType(numericExp)
+      const result = sut.getExpressionType(numericExp.str)
       expect(result).toBe('numeric')
+    }
+  })
+
+  test('Should detect numeric expressions number of terms correctly', async () => {
+    const sut = new AlgebraicExpressionService()
+    for (const numericExp of numericExpressionsExamples) {
+      const result = sut.getNumOfTerms(numericExp.str)
+      expect(result).toBe(numericExp.numOfTerms)
     }
   })
 })
