953ac513a23e0178bc827b3c2be32ae024bd0ec8
Breno Alberto <brenoaps435@gmail.com>
2022-07-26 19:26:19 -0300

    test: ensure AlgebraicExpressionService detects monomial expressions correctly

--- src/precalc/algebra/algebraicExpression/algebraicExpression.spec.ts
+++ src/precalc/algebra/algebraicExpression/algebraicExpression.spec.ts
@@ -10,7 +10,7 @@ const numericExpressionsExamples = [
     numOfTerms: 2
   },
   {
-    str: '1 / 5 + 8',
+    str: '1 / -5 + 8',
     numOfTerms: 3
   },
   {
@@ -20,6 +20,25 @@ const numericExpressionsExamples = [
   {
     str: '1^5 / 2^5',
     numOfTerms: 2
+  }
+]
+
+const monomialExpExamples = [
+  {
+    str: 'x',
+    numOfTerms: 1
+  },
+  {
+    str: '5x^2',
+    numOfTerms: 1
+  },
+  {
+    str: 'p^3q',
+    numOfTerms: 1
+  },
+  {
+    str: '-6ty',
+    numOfTerms: 1
   }
 ]
 
@@ -37,6 +56,22 @@ describe('AlgebraicExpressionService', () => {
     for (const numericExp of numericExpressionsExamples) {
       const result = sut.getNumOfTerms(numericExp.str)
       expect(result).toBe(numericExp.numOfTerms)
+    }
+  })
+
+  test('Should detect monomial expressions correctly', async () => {
+    const sut = new AlgebraicExpressionService()
+    for (const monomialExp of monomialExpExamples) {
+      const result = sut.getExpressionType(monomialExp.str)
+      expect(result).toBe('monomial')
+    }
+  })
+
+  test('Should detect monomial expressions number of terms correctly', async () => {
+    const sut = new AlgebraicExpressionService()
+    for (const monomialExp of monomialExpExamples) {
+      const result = sut.getNumOfTerms(monomialExp.str)
+      expect(result).toBe(monomialExp.numOfTerms)
     }
   })
 })
