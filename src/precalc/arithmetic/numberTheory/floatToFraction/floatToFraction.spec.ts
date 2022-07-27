c7b6e2b8a17c21cc481de3d6499836ecc7468ce9
Breno Alberto <brenoaps435@gmail.com>
2022-07-27 15:58:12 -0300

    test: ensure float is correctly converted to fraction

---
+++ src/precalc/arithmetic/numberTheory/floatToFraction/floatToFraction.spec.ts
@@ -0,0 +1,9 @@
+import { FloatToFraction } from './floatToFraction'
+
+describe('FloatToFraction', () => {
+  test('Should convert float to fraction correctly', async () => {
+    const sut = new FloatToFraction()
+    const result = sut.convert(0.666666667)
+    expect(result).toEqual([2, 3])
+  })
+})
